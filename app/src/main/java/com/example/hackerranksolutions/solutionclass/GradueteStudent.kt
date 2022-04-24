37025d599b9501563b115831aa697cdb3f575bec
esa <Shire123>
2022-04-24 16:51:29 +0300

    added solution of GradueteStudent question

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/GradueteStudent.kt
@@ -0,0 +1,30 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+
+    val result = gradingStudents(arrayOf(73, 67, 38, 33))
+
+    println(result.joinToString("\n"))
+
+}
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+
+    val updatedList = mutableListOf<Int>()
+
+    for (grade in grades) {
+        when {
+            grade % 5 == 3 && grade >= 38 -> {
+                updatedList.add(grade + 2)
+            }
+            grade % 5 == 4 && grade >= 38 -> {
+                updatedList.add(grade + 1)
+            }
+            else -> {
+                updatedList.add(grade)
+            }
+        }
+    }
+    return updatedList.toTypedArray()
+
+}
