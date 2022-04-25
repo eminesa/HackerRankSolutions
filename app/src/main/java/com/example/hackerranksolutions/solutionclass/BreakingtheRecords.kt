a7e17eddc1b124e196109db6ab384000eb33422d
esa <Shire123>
2022-04-26 02:14:41 +0300

    added solution of BreakingtheRecords question

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/BreakingtheRecords.kt
@@ -0,0 +1,55 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+
+ println(breakingRecords(arrayOf(3 ,4, 21, 36, 10, 28, 35, 5, 24, 42)))
+
+}
+
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+
+    var minScore = if (scores.isNotEmpty()) scores[0] else 0
+    var maxScore = if (scores.isNotEmpty()) scores[0] else 0
+    var minScoreCounter = 0
+    var maxScoreCounter = 0
+
+    for (score in scores) {
+
+        if (score<minScore){
+            minScore = score
+            minScoreCounter += 1
+        }
+
+        if (score>maxScore){
+            maxScore = score
+            maxScoreCounter += 1
+        }
+    }
+
+    return arrayOf(maxScoreCounter, minScoreCounter)
+
+}
+
+fun breakingRecords1(scores: Array<Int>): Array<Int> {
+
+    var minScore = scores.first()
+    var maxScore = scores.first()
+    var minScoreCounter = 0
+    var maxScoreCounter = 0
+
+
+    scores.map { score ->
+        if (score<minScore){
+            minScore = score
+            minScoreCounter += 1
+        }
+
+        if (score>maxScore){
+            maxScore = score
+            maxScoreCounter += 1
+        }
+    }
+
+    return arrayOf(maxScoreCounter, minScoreCounter)
+
+}
