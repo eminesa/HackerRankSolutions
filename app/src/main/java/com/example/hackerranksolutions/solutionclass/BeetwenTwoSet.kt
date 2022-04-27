e97b0730b3ee80a7b97975b29d9c59b0dbdbef13
esa <Shire123>
2022-04-27 21:34:08 +0300

    added the BeetwenTwoSet solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/BeetwenTwoSet.kt
@@ -0,0 +1,36 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+    println(getTotalX(arrayOf(2, 6), arrayOf(24, 36)))
+}
+
+fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
+    var total = 0
+
+    (1..100).map{ index->
+
+        var status = true
+        for (element in b) {
+            // I used for instead map in this line
+            if (element % index != 0) {
+                status = false
+                break
+            }
+        }
+
+        if (status) {
+            for (element in a) {
+                //for the same reason as above
+                if (index % element != 0) {
+                    status = false
+                    break
+                }
+            }
+        }
+        if (status) {
+            total++
+        }
+    }
+    return total
+}
+
