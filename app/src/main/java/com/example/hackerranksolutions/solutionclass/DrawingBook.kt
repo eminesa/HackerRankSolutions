83628627a8107982b74a81a62f7a0ff2dc4ba2e3
esa <Shire123>
2022-04-29 23:40:28 +0300

    added the DrawingBook problem solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/DrawingBook.kt
@@ -0,0 +1,19 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+    println(pageCount(6, 2))
+}
+
+fun pageCount(n: Int, p: Int): Int {
+
+    return if (n == 2)
+        0
+    else if (n % 2 == 0 && n - p == 1)
+        1
+    else {
+        //Returns the smaller of two values.
+        //If either value is NaN, then the result is NaN.
+        Math.min(p / 2, (n - p) / 2)
+    }
+
+}
