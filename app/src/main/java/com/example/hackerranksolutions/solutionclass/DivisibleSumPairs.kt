dc89170e173dce7e3e675e1395e1019a7ddd2640
esa <Shire123>
2022-05-01 20:46:10 +0300

    updated DivisibleSumPairs solution

--- app/src/main/java/com/example/hackerranksolutions/solutionclass/DivisibleSumPairs.kt
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/DivisibleSumPairs.kt
@@ -2,7 +2,6 @@ package com.example.hackerranksolutions.solutionclass
 
 fun main() {
     println(divisibleSumPairs(3, arrayOf(1, 3, 2, 6, 1, 2)))
-    println(divisibleSumPairs1(3, arrayOf(1, 3, 2, 6, 1, 2)))
 }
 
 fun divisibleSumPairs(k: Int, ar: Array<Int>): Int {
@@ -23,28 +22,6 @@ fun divisibleSumPairs(k: Int, ar: Array<Int>): Int {
 
             }
 
-        }
-    }
-
-    return sum
-}
-
-fun divisibleSumPairs1(k: Int, ar: Array<Int>): Int {
-
-    var sum = 0
-
-    ar.mapIndexed { arrIndex, i ->
-
-        ar.mapIndexed { index, i ->
-            if (arrIndex != index && arrIndex < index) {
-
-                val isDivided = ((ar[arrIndex] + ar[index]) % k) == 0
-
-                if (isDivided) {
-                    sum += 1
-                }
-
-            }
         }
     }
 
