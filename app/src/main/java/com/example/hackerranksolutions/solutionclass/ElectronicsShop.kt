a0cbd1205e8156db3df072a39cb6eab99bea02b7
esa <Shire123>
2022-05-09 18:28:45 +0300

    Added ElectronicsShop solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/ElectronicsShop.kt
@@ -0,0 +1,27 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+
+    println(getMoneySpent(arrayOf(40, 50, 60), arrayOf(5, 8, 12), 60))
+
+}
+
+fun getMoneySpent(keyboards: Array<Int>, drives: Array<Int>, b: Int): Int {
+
+    var result: Int = -1
+    keyboards.map { keyboardPrice ->
+        drives.map { drivePrice ->
+
+            val total = keyboardPrice + drivePrice
+
+            result = if (total in (result + 1)..b) {
+                total
+            } else {
+                result
+            }
+        }
+    }
+
+    return result
+
+}
