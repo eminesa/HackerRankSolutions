951e33447c7f409f9658473ea8fb17a6ca7a8fca
esa <Shire123>
2022-05-13 19:33:43 +0300

    added AlternatingCharacters solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/AlternatingCharacters.kt
@@ -0,0 +1,28 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+
+println(alternatingCharacters("ABBAAAABBA"))
+
+}
+
+fun alternatingCharacters(s: String): Int {
+
+    val charList = s.toCharArray()
+    var letterOfControl = '?'
+    var deleteLetterSize = 0
+
+    charList.map { letter ->
+
+        when (letter) {
+            letterOfControl -> {
+                deleteLetterSize += 1
+            }
+            else -> {
+                letterOfControl = letter
+            }
+        }
+    }
+
+    return deleteLetterSize
+}
