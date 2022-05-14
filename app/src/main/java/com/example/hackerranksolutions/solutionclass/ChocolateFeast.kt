090f4fd20efbe0875e10590bf20591d696d72d62
esa <Shire123>
2022-05-14 15:18:44 +0300

    added ChocolateFeast solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutionclass/ChocolateFeast.kt
@@ -0,0 +1,22 @@
+package com.example.hackerranksolutions.solutionclass
+
+fun main() {
+
+    println(chocolateFeast(n = 6, c = 2, m = 2))
+
+}
+
+fun chocolateFeast(n: Int, c: Int, m: Int): Int {
+
+    val chocs = n / c
+    var wrapper = chocs
+    var exChocs = wrapper / m
+
+    while (wrapper >= m) {
+        exChocs += (wrapper / m + wrapper % m) / m
+        wrapper /= m
+    }
+
+    return exChocs + chocs
+
+}
