6db88051abfc27f53f584bf50a08efda4e2c1a3d
esa <Shire123>
2022-05-15 22:39:38 +0300

    added MissingNumbers solution

---
+++ app/src/main/java/com/example/hackerranksolutions/solutions/MissingNumbers.kt
@@ -0,0 +1,34 @@
+package com.example.hackerranksolutions
+
+fun main() {
+
+    val result = missingNumbers(
+        arrayOf(203, 204, 205, 206, 207, 208, 203, 204, 205, 206),
+        arrayOf(203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204)
+    )
+
+    println(result.joinToString(","))
+
+}
+
+fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
+
+    val foundOneTimeList: MutableList<Int> = brr.toMutableList()
+
+    for (indexOfFirst in arr.indices) {
+        for (indexOfSecond in foundOneTimeList.indices) {
+            if (arr[indexOfFirst] == foundOneTimeList[indexOfSecond]) {
+                foundOneTimeList.remove(arr[indexOfFirst])
+                break
+            }
+        }
+    }
+
+    return foundOneTimeList.sorted().distinct().toTypedArray()
+}
+
+
+/*  repeat kullandığımızda içinde filter kullanmamıza izin vermiyor.
+repeat(arr.size) { indexOfFirst ->
+      anArray = foundOneTimeList.filter { indexOfSecond -> arr[indexOfFirst] != indexOfSecond }
+  } */
