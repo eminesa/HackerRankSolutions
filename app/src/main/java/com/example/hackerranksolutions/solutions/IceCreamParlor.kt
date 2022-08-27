e25fe6616e32bc3bd0e5bcddacf315b4e5d70209
eminesa <eminesa34@gmail.com>
2022-08-27 21:54:00 +0300

    updated IceCreamParlor.kt question

--- app/src/main/java/com/example/hackerranksolutions/solutions/IceCreamParlor.kt
+++ app/src/main/java/com/example/hackerranksolutions/solutions/IceCreamParlor.kt
@@ -2,10 +2,9 @@ package com.example.hackerranksolutions.solutions
 
 fun main() {
    // val result = icecreamParlor(4, arrayOf(1, 4, 5, 3, 2))
-    val result = icecreamParlor(4, arrayOf(2, 2,4, 3))
+    val result = icecreamParlor(4, arrayOf(2, 2,4, 1))
     println(result.joinToString(" "))
 }
-
 
 fun icecreamParlor(m: Int, arr: Array<Int>): Array<Int> {
 
@@ -13,11 +12,15 @@ fun icecreamParlor(m: Int, arr: Array<Int>): Array<Int> {
 
     for (firstPrice in arr.indices) {
 
-        for (secondPrice in firstPrice + 1 until arr.size) {
+        for (secondPrice in arr.indices) {
+            if (firstPrice == secondPrice){
+                continue
+            }
             if (arr[firstPrice] + arr[secondPrice] == m) {
                 list.clear()
-                list.add(firstPrice+1)
                 list.add(secondPrice+1)
+                list.add(firstPrice+1)
+                break
             }
         }
     }
