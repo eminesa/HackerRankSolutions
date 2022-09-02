7442d9810273a0e9f55bb55e54e3922ac7d3ab99
eminesa <eminesa34@gmail.com>
2022-09-02 22:36:54 +0300

    updated TheTimeInWords.kt question

--- app/src/main/java/com/example/hackerranksolutions/solutions/TheTimeInWords.kt
+++ app/src/main/java/com/example/hackerranksolutions/solutions/TheTimeInWords.kt
@@ -2,60 +2,33 @@ package com.example.hackerranksolutions.solutions
 
 
 fun main() {
-println(timeInWords(5,47))
+    println(timeInWords(5, 47))
 }
 
 fun timeInWords(h: Int, m: Int): String {
-   var time = ""
+
     val stringOfTime = arrayOf(
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "twelve",
-        "thirteen",
-        "fourteen",
-        "fifteen",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen",
-        "twenty",
-        "twenty one",
-        "twenty two",
-        "twenty three",
-        "twenty four",
-        "twenty five",
-        "twenty six",
-        "twenty seven",
-        "twenty eight",
-        "twenty nine"
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+        "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three",
+        "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine"
     )
+    //thirteen minutes to six
 
-    if(m<=30){
-        time = when (m) {
-            0 -> stringOfTime[h]+" o' clock"
-            15 -> "quarter past "+stringOfTime[h]
-            30 -> "half past "+stringOfTime[h]
-            1 -> stringOfTime[m]+" minute past "+stringOfTime[h]
-            else -> stringOfTime[m]+" minutes past "+stringOfTime[h]
+    return if (m <= 30) {
+        when (m) {
+            0 -> stringOfTime[h] + " o' clock"
+            15 -> "quarter past " + stringOfTime[h]
+            30 -> "half past " + stringOfTime[h]
+            1 -> stringOfTime[m] + " minute past " + stringOfTime[h]
+            else -> stringOfTime[m] + " minutes past " + stringOfTime[h]
+        }
+    } else {
+        when (m) {
+            45 -> "quarter to " + stringOfTime[h + 1]
+            59 -> stringOfTime[60 - m] + " minute to " + stringOfTime[h + 1]
+            else -> stringOfTime[60 - m] + " minutes to " + stringOfTime[h + 1]
         }
     }
-    else{
-        time = when (m) {
-            45 -> "quarter to "+stringOfTime[h+1]
-            59 -> stringOfTime[60-m]+" minute to "+stringOfTime[h+1]
-            else -> stringOfTime[60-m]+" minutes to "+stringOfTime[h+1]
-        }
-    }
 
-    return time
 }
