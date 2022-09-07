78f7f0bcd0fa761d22ad93e62a9b23dea6a8c8cd
esa <Shire123>
2022-09-07 13:54:32 +0300

    added same solution

---
+++ app/src/main/java/com/example/hackerranksolutions/leet/MoveZeros.kt
@@ -0,0 +1,25 @@
+package com.example.hackerranksolutions.leet
+
+fun main() {
+    println(moveZeroes(intArrayOf(0, 1, 0, 3, 12)))
+}
+fun moveZeroes(nums: IntArray): Unit {
+
+    val moveZeroList: MutableList<Int> = nums.toMutableList()
+    var counter = nums.size - 1
+    var index = 0
+    while (index < counter) {
+
+        if (moveZeroList[index] == 0) {
+            moveZeroList.removeAt(index)
+            moveZeroList.add(0)
+            counter--
+        } else {
+            index++
+        }
+    }
+
+    for (i in nums.indices) {
+        nums[i] = moveZeroList[i]
+    }
+}
---
+++ app/src/main/java/com/example/hackerranksolutions/leet/SquaresOfSortedList.kt
@@ -0,0 +1,81 @@
+package com.example.hackerranksolutions.leet
+
+fun main() {
+    val result = sortedSquares1(intArrayOf(-4, 1, 7, 18))
+
+    println(result.joinToString(" "))
+}
+
+fun sortedSquares2(nums: IntArray): IntArray { // Time exeption yedik iyi mi :(
+
+    if (nums.isEmpty()) return nums
+    val squareList: MutableList<Int> = ArrayList()
+
+    for (numberIndex in nums.indices) {
+        val squareNumber = nums[numberIndex] * nums[numberIndex]
+        squareList.add(squareNumber)
+    }
+
+    val size = squareList.size
+    for (i in 0 until size) {
+        var minValue = squareList[i]
+        var index = i + 1
+        while (index < size) {
+            if (squareList[index] < minValue) {
+                val temporary = squareList[index]
+                squareList[index] = minValue
+                squareList[i] = temporary
+                minValue = temporary
+            }
+            index++
+        }
+    }
+
+    return squareList.toIntArray()
+}
+
+fun sortedSquares1(nums: IntArray): IntArray { // istedigim sonucu vermiyor.
+
+    if (nums.isEmpty()) return nums
+    val squareList: MutableList<Int> = ArrayList()
+    val result: MutableList<Int> = ArrayList()
+
+    for (numberIndex in nums.indices) {
+        val squareNumber = nums[numberIndex] * nums[numberIndex]
+        squareList.add(squareNumber)
+
+        if (result.isEmpty()) {
+            result.add(squareNumber)
+        } else {
+            val minValue = result.last()
+            if (squareNumber < minValue) {
+                val resultSize = result.size - 1
+                result[resultSize] = squareNumber
+                result.add(minValue)
+            } else {
+                result.add(squareNumber)
+            }
+        }
+    }
+    return result.toIntArray()
+}
+
+fun sortedSquares(nums: IntArray): IntArray { // Sonunda calısan bir cozum
+    val n = nums.size
+    val result = IntArray(n)
+    var left = 0
+    var right = n - 1
+    for (i in n - 1 downTo 0) {
+        var square: Int
+
+        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+            square = nums[right]
+            right--
+        } else {
+            square = nums[left]
+            left++
+        }
+        result[i] = square * square
+    }
+    return result
+}
