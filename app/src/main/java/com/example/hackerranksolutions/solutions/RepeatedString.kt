8df7a2ffd752de08dcbe9c87d400c47d4635e911
esa <Shire123>
2022-09-18 12:03:09 +0300

    updated RepeatedString.kt solution

--- app/src/main/java/com/example/hackerranksolutions/solutions/RepeatedString.kt
+++ app/src/main/java/com/example/hackerranksolutions/solutions/RepeatedString.kt
@@ -6,17 +6,15 @@ fun main() {
 
 fun repeatedString(s: String, n: Long): Long {
 
-    val lengthOfLetter: Long
     val remainingValue = n % s.length
     val aCount = s.count { it == 'a' }
 
-
-    if (remainingValue == 0L) {
-        lengthOfLetter = aCount * (n / s.length)
+    val lengthOfLetter: Long = if (remainingValue == 0L) {
+        aCount * (n / s.length)
     } else {
         val myString = s.subSequence(0, remainingValue.toInt())
 
-        lengthOfLetter = myString.count { it == 'a' }.toLong() + aCount * (n / s.length)
+        myString.count { it == 'a' }.toLong() + aCount * (n / s.length)
     }
 
     return lengthOfLetter
