2031ce33a37c59a4a9c0819b2d8312418562eed6
esa <Shire123>
2022-09-28 18:16:17 +0300

    added same problem solution

---
+++ app/src/main/java/com/example/hackerranksolutions/leet/RemoveLinkedListElement.kt
@@ -0,0 +1,34 @@
+package com.example.hackerranksolutions.leet
+
+fun main() {
+
+    val listNode = ListNode(1)
+    listNode.next = ListNode(2)
+    listNode.next?.next = ListNode(2)
+    listNode.next?.next?.next = ListNode(1)
+
+    removeElements(listNode, 2)
+}
+
+fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+
+    if (head == null) return head
+
+    var currentHead = head
+    val sentinel = ListNode(0)
+    sentinel.next = head
+    var previous = sentinel
+
+
+    while (currentHead != null) {
+        if (currentHead.`val` == `val`) {
+            previous.next = currentHead.next
+        } else{
+            previous = currentHead
+        }
+
+        currentHead = currentHead.next
+    }
+
+    return sentinel.next
+}
