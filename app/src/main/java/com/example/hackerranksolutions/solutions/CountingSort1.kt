f1bd88a9a2495348fc2f01831d9ca67f85002bfb
esa <Shire123>
2022-10-08 20:07:53 +0300

    added same problem solution

--- app/src/main/java/com/example/hackerranksolutions/solutions/CountingSort1.kt
+++ app/src/main/java/com/example/hackerranksolutions/solutions/CountingSort1.kt
@@ -1,25 +1,21 @@
 package com.example.hackerranksolutions.solutions
 
 fun main() {
-
-    val result= countingSort(arrayOf(19, 10, 12, 10, 24, 25, 22))
-    println(result.joinToString(" "))
-
+    //val result = countingSort(arrayOf(19, 10, 12, 10, 24, 25, 22))
+    val result = countingSort(arrayOf(1, 1, 3, 2, 1))
+    println(result.joinToString(", "))
 }
 
 fun countingSort(arr: Array<Int>): Array<Int> {
     val freqArray: MutableList<Int> = ArrayList()
-    var value: Int
-    var freq: Int
 
     for (i in 0..99) {
         freqArray.add(0)
     }
 
     for (element in arr) {
-        value = element
-        freq = freqArray[value]
-        freqArray[value] = freq + 1
+        val freq = freqArray[element]
+        freqArray[element] = freq + 1
     }
 
     return freqArray.toTypedArray()
